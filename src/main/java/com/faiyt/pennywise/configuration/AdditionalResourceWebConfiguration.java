e8030c96676f552d816ca79c7de4b7a7f975923a
unfaiyted <unfaiyted@gmail.com>
2018-06-08 08:48:24 -0500

    updating example prop

--- src/main/java/com/faiyt/pennywise/configuration/AdditionalResourceWebConfiguration.java
+++ src/main/java/com/faiyt/pennywise/configuration/AdditionalResourceWebConfiguration.java
@@ -3,6 +3,7 @@ package com.faiyt.pennywise.configuration;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -25,5 +26,9 @@ public class AdditionalResourceWebConfiguration implements WebMvcConfigurer {
                 .allowedOrigins("*");
     }
 
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        // registry.addInterceptor();
+    }
 }
 
